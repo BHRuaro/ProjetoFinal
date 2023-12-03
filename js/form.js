54e70c5161bdf1821ae7016e8d1853ddb45d2d77
Bruno Ruaro <brunoruaro@alunos.utfpr.edu.br>
2023-12-03 09:31:22 -0300

    validacao formulario

--- js/form.js
+++ js/form.js
@@ -6,7 +6,12 @@ const spanErrors = document.querySelectorAll('.error-message');
 const messages = {
     shortName: "O campo nome deve ter no mínimo 2 caracteres",
     emptyfield: "Preencha o campo {field}",
-    invalidEmail: "Informe um endereço de email válido."
+    invalidEmail: "Informe um endereço de email válido.",
+    invalidPhone: "Informe um número de telefone válido.",
+    invalidCPF: "Informe um número de CPF válido.",
+    invalidSex: "Informe o Sexo.",
+    invalidSenha: "A senha deve conter ao menos uma letra e um dígito e possuir no mínimo 8 caracteres.",
+    invalidConfSenha: "As senhas não conferem."
 }
 
 function validateInput(input, spanError) {
@@ -15,18 +20,36 @@ function validateInput(input, spanError) {
     if (input.value.trim() === '') {
         error = true;
         message = messages.emptyfield.replace('{field}', input.name)
-    } else if (input.name === 'nome' && input.value.length < 2) {
+    } else if (input.name === 'Nome' && input.value.length < 2) {
         error = true;
         message = messages.shortName;
-    } else if (input.name === 'email' && !/\S+@\S+\.\S+/.test(input.value)) {
+    } else if (input.name === 'Sobrenome' && input.value.length < 2) {
+        error = true;
+        message = messages.shortName;
+    } else if (input.name === 'Email' && !/\S+@\S+\.\S+/.test(input.value)) {
         error = true;
         message = messages.invalidEmail;
+    } else if (input.name === 'Telefone' && !/^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/.test(input.value)) {
+        error = true;
+        message = messages.invalidPhone;
+    } else if (input.name === 'C.P.F.' && !/^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/.test(input.value)) {
+        error = true;
+        message = messages.invalidCPF;
+    } else if (input.name === 'Sexo' && input.value === 'Selecione uma opção') {
+        error = true;
+        message = messages.invalidSex;
+    } else if (input.name === 'Senha' && !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(input.value)) {
+        error = true;
+        message = messages.invalidSenha;
+    } else if (input.name === 'ConfSenha' && input.value !== document.querySelector('#senha').value) {
+        error = true;
+        message = messages.invalidConfSenha;
     }
 
     if (error) {
         spanError.textContent = message;
         spanError.style.display = 'block';
-        spanError.classList.add('errofont');
+        spanError.classList.add('text-danger');
         input.classList.add('erroinput');
     }
     else {
@@ -38,7 +61,7 @@ function validateInput(input, spanError) {
 }
 
 form.addEventListener('submit', function (event) {
-    e.preventDefault();
+    event.preventDefault();
     inputs.forEach(function (input, index) {
         console.log(index);
         if (index < spanErrors.length) {
